6cae11ebdfcee74f603321cd84306f24ee888538
hacktroopers <32078333+hacktroopers@users.noreply.github.com>
2017-09-25 14:27:43 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,205 +1,89 @@
 'use strict';
 
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
- * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
- * testing instructions are located at http://amzn.to/1LzFrj6
- *
- * For additional samples, visit the Alexa Skills Kit Getting Started guide at
- * http://amzn.to/1LGWsLG
- */
+var Alexa = require('alexa-sdk');
 
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context, callback);
+	alexa.appId = "amzn1.ask.skill.f63c8bdf-dce3-43a8-abd0-c6e712218eb3";
+	alexa.dynamoDBTableName = 'Inspections';
+	alexa.registerHandlers(handlers);
+	alexa.execute();
+};
 
-// --------------- Helpers that build all of the responses -----------------------
-
-function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: 'PlainText',
-            text: output,
-        },
-        card: {
-            type: 'Simple',
-            title: `SessionSpeechlet - ${title}`,
-            content: `SessionSpeechlet - ${output}`,
-        },
-        reprompt: {
-            outputSpeech: {
-                type: 'PlainText',
-                text: repromptText,
-            },
-        },
-        shouldEndSession,
-    };
+function getNewInspectionObject(customer, vehicle) {
+	var inspectionObject = {};
+	var date = new Date();
+  	var inspectionId = Math.floor(Math.random() * 1000);
+    inspectionObject = {"id":inspectionId, "customer":customer, "vehicle":vehicle, "date":date, "inspections": []};
+    return inspectionObject;
 }
 
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: '1.0',
-        sessionAttributes,
-        response: speechletResponse,
-    };
+function markVehicleCondition(sessionAttributes, inspectionItem, condition) {
+	var inspection = sessionAttributes["mpiInspection"];
+	var inspectionMap = {};
+	inspectionMap[inspectionItem] = condition;
+	inspection['inspections'].push(inspectionMap);
+    console.log("Marked condition" + JSON.stringify(inspection));
+    sessionAttributes['mpiInspection'] = inspection;
 }
 
+var handlers = {
+    "LaunchRequest": function () {
+		this.emit(':ask',
+		'Hello. Welcome to Alexa voice enabled multi point inspection. Please say, Start new multi point inspection or Resume inspection');
+	},
+	'Unhandled': function () {
+        this.emit(':ask', "I dint get that", "I dint get that");
+	},
 
-// --------------- Functions that control the skill's behavior -----------------------
-
-function getWelcomeResponse(callback) {
-    // If we wanted to initialize the session to have some attributes we could add those here.
-    const sessionAttributes = {};
-    const cardTitle = 'Welcome';
-    const speechOutput = 'Welcome to the Alexa voice enabled multi point inspection ' +
-        'To get started, say the inspection item ,followed by its condition. For eg, Mark battery condition as fair';
-    // If the user either does not reply to the welcome message or says something that is not
-    // understood, they will be prompted again with this text.
-    const repromptText = 'Say Mark Battery condition as fair';
-    const shouldEndSession = false;
-
-    callback(sessionAttributes,
-        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-}
-
-function handleSessionEndRequest(callback) {
-    const cardTitle = 'Session Ended';
-    const speechOutput = 'Thank you for trying the Alexa voice enabled multi point inspection. Have a nice day!';
-    // Setting this to true ends the session and exits the skill.
-    const shouldEndSession = true;
-
-    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
-}
-
-function createFavoriteColorAttributes(favoriteColor) {
-    return {
-        favoriteColor,
-    };
-}
-
-/**
- * Sets the color in the session and prepares the speech to reply to the user.
- */
-function markInspectionCondition(intent, session, callback) {
-    const cardTitle = intent.name;
-    const inspectionItemSlot = intent.slots.InspectionItem;
-    const conditionSlot = intent.slots.Condition;
-    let repromptText = '';
-    let sessionAttributes = session.attributes;
-    let availableTimes = '';
-    const shouldEndSession = false;
-    let speechOutput = '';
-
-    if (inspectionItemSlot) {
-        const inspectionItemName = inspectionItemSlot.value;
-        const condition = conditionSlot.value;
-        var sessionKey = inspectionItemName;
-        sessionAttributes[sessionKey] = condition;
-        speechOutput = `Marked `;
-        for (var key in sessionAttributes) {
-          if (sessionAttributes.hasOwnProperty(key)) {
-            console.log(key + " -> " + sessionAttributes[key]);
-            speechOutput = speechOutput +` ${key} as ${sessionAttributes[key]}`;
-          }
+    'StartNewMPIInspection': function () {
+        var customer = this.event.request.intent.slots.Customer.value;
+        var vehicle = this.event.request.intent.slots.Vehicle.value;
+        var updatedIntent = this.event.request.intent;
+        
+        if(customer == null) {
+        	return this.emit(':elicitSlot', "Customer", "What is the customer's name?", "Please provide customer's full name", updatedIntent);
         }
-        repromptText = "Do you want continue inspection?";
-    } else {
-        speechOutput = "I dint get that. Please try again.";
-        repromptText = "I dint get what you are saying. You can say " +
-            ' Mark Battery condition good';
+        if(vehicle == null) {
+        	return this.emit(':elicitSlot', "Vehicle", "What is the vehicle make model year?", "Please provide Vehicle make, model and year", updatedIntent);
+        }
+        var inspection = getNewInspectionObject(customer, vehicle);
+        var sessionAttributes = this.event.session.attributes;
+        sessionAttributes["mpiInspection"] = inspection;
+        console.log("New inspection object:" + JSON.stringify(inspection));
+        
+        return this.emit(':ask', 'Starting new multi point inspection for ' + vehicle + ' owned by ' + customer + ', start registering vehicle condition',
+        'Please say something like Mark battery condition as good');
+
+    },
+    
+    'MarkInspectionCondition': function () {
+    	var inspectionItem = this.event.request.intent.slots.InspectionItem.value;
+    	var condition = this.event.request.intent.slots.Condition.value;
+    	var updatedIntent = this.event.request.intent;
+    	var sessionAttributes = this.event.session.attributes;
+    	if(inspectionItem == null) {
+    		return this.emit(':elicitSlot', "InspectionItem", "What is the inspection item name?", "Please provide inspection item name", updatedIntent);
+    	}
+    	if(condition == null) {
+    		return this.emit(':elicitSlot', "Condition", "What is the condition?", "Please provide vehicle condition", updatedIntent);
+    	}
+    	markVehicleCondition(sessionAttributes, inspectionItem, condition);
+    	
+    	return this.emit(':ask', 'Marked ' + inspectionItem + ' as ' + condition);
+    },
+    
+    'ResumeInspection': function() {
+    	
+    },
+    
+    'AMAZON.StopIntent': function() {
+        var sessionAttributes = this.event.session.attributes;
+    	var inspectionObject = sessionAttributes["currentInspection"];  
+    	this.attributes['yourAttribute'] = 'value';
+        console.log("TODO Persist inspection object:" + JSON.stringify(inspection));
+    	return this.emit(':tell', 'Multi point inspection saved successfully');
+    	
     }
 
-    callback(sessionAttributes,
-         buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-}
-
-
-// --------------- Events -----------------------
-
-/**
- * Called when the session starts.
- */
-function onSessionStarted(sessionStartedRequest, session) {
-    console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
-}
-
-/**
- * Called when the user launches the skill without specifying what they want.
- */
-function onLaunch(launchRequest, session, callback) {
-    console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
-
-    // Dispatch to your skill's launch.
-    getWelcomeResponse(callback);
-}
-
-/**
- * Called when the user specifies an intent for this skill.
- */
-function onIntent(intentRequest, session, callback) {
-    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
-
-    const intent = intentRequest.intent;
-    const intentName = intentRequest.intent.name;
-
-    // Dispatch to your skill's intent handlers
-    if (intentName === 'MarkInspectionCondition') {
-        markInspectionCondition(intent, session, callback);
-    } else if (intentName === 'AMAZON.HelpIntent') {
-        getWelcomeResponse(callback);
-    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
-        handleSessionEndRequest(callback);
-    } else {
-        throw new Error('Invalid intent');
-    }
-}
-
-/**
- * Called when the user ends the session.
- * Is not called when the skill returns shouldEndSession=true.
- */
-function onSessionEnded(sessionEndedRequest, session) {
-    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
-    // Add cleanup logic here
-}
-
-
-// --------------- Main handler -----------------------
-
-// Route the incoming request based on type (LaunchRequest, IntentRequest,
-// etc.) The JSON body of the request is provided in the event parameter.
-exports.handler = (event, context, callback) => {
-    try {
-        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
-
-        /**
-         * Uncomment this if statement and populate with your skill's application ID to
-         * prevent someone else from configuring a skill that sends requests to this function.
-         */
-        /*
-        if (event.session.application.applicationId !== 'amzn1.echo-sdk-ams.app.[unique-value-here]') {
-             callback('Invalid Application ID');
-        }
-        */
-
-        if (event.session.new) {
-            onSessionStarted({ requestId: event.request.requestId }, event.session);
-        }
-
-        if (event.request.type === 'LaunchRequest') {
-            onLaunch(event.request,
-                event.session,
-                (sessionAttributes, speechletResponse) => {
-                    callback(null, buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === 'IntentRequest') {
-            onIntent(event.request,
-                event.session,
-                (sessionAttributes, speechletResponse) => {
-                    callback(null, buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === 'SessionEndedRequest') {
-            onSessionEnded(event.request, event.session);
-            callback();
-        }
-    } catch (err) {
-        callback(err);
-    }
 };
